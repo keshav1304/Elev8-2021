a06d593c6e1b0d015c7952fc8f613f7027115997
Godlysk <shivkampani@gmail.com>
2021-02-19 09:05:19 +0530

    Init Commit
    :rocket:

---
+++ Test2021/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,68 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.DriveCommand;
+import frc.robot.subsystems.DriveSubsystem;
+import edu.wpi.first.wpilibj.Joystick;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+
+  // The robot's subsystems and commands are defined here...
+  private final DriveSubsystem driveSubsystem = new DriveSubsystem();
+
+  // Commands
+  private final DriveCommand driveCommand = new DriveCommand(driveSubsystem);
+
+  // IO Devices
+  public static Joystick joy1 = new Joystick(1);
+
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the button bindings
+    configureButtonBindings();
+
+    driveSubsystem.setDefaultCommand(driveCommand);
+  }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {}
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An ExampleCommand will run in autonomous
+    return null;
+  }
+
+  public static double getY(Joystick joy, double deadband) {
+    double value = -1 * joy.getY();
+    if (Math.abs(value) < deadband) return 0;
+    return value;
+  }
+
+  public static double getZ(Joystick joy, double deadband) {
+    double value = joy.getZ();
+    if (Math.abs(value) < deadband) return 0;
+    return value;
+  }
+
+}
---
+++ Test2021/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,54 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.Constants;
+import frc.robot.RobotContainer;
+
+public class DriveSubsystem extends SubsystemBase {
+
+  private final WPI_TalonSRX FR;
+  private final WPI_TalonSRX BR;
+  private final SpeedControllerGroup rightSide;
+
+  private final WPI_TalonSRX FL;
+  private final WPI_TalonSRX BL;  
+  private final SpeedControllerGroup leftSide;
+
+  private final DifferentialDrive driveTrain;
+
+  /** Creates a new DriveSubsystem. */
+  public DriveSubsystem() {
+
+    FR = new WPI_TalonSRX(Constants.FR_port);
+    BR = new WPI_TalonSRX(Constants.BR_port);
+    rightSide = new SpeedControllerGroup(FR, BR);
+    
+    FL = new WPI_TalonSRX(Constants.FL_port);
+    BL = new WPI_TalonSRX(Constants.BL_port); 
+    leftSide = new SpeedControllerGroup(FL, BL);
+    
+    driveTrain = new DifferentialDrive(leftSide, rightSide);
+    
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void arcadeInbuilt(final double y, final double z) {
+    FR.setInverted(false);
+    BR.setInverted(false);
+
+    driveTrain.arcadeDrive(y * Constants.maxSpeed, z * Constants.maxSpeed);
+  }
+}
